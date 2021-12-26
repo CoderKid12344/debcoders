12cd53dba70d474d00792a6edf8da11eaa35b812
CoderKid12344 <dipalidutta312@gmail.com>
2021-12-26 18:07:02 +0530

    fghui

--- home/admin.py
+++ home/admin.py
@@ -4,8 +4,7 @@ from home.models import Blog, Contact
 # Register your models here.
 admin.site.register(Contact)
 
-# @admin.register(Blog)
-# class BlogAdmin(admin.ModelAdmin):
-#     class Media:
-#         js = ('js/tinyinject.js',)
-admin.site.register(Blog)
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('js/tinyinject.js',)
