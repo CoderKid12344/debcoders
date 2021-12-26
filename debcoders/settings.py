750f16737123f038ee5f75a71b6ab81e8c231d8a
CoderKid12344 <dipalidutta312@gmail.com>
2021-12-26 17:16:56 +0530

    third

--- debcoders/settings.py
+++ debcoders/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
@@ -133,3 +133,5 @@ MESSAGE_TAGS = {
     messages.INFO: '',
     50: 'critical',
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
